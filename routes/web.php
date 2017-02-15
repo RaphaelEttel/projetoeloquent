502d88b93fbc4fc38dc52e62522b6d3af8bfae92
php 08 <php.08@metralha.cec.ime.usp.br>
2017-02-15 21:56:10 -0200

    Acrescentando listagem e detalhes dos livros das editoras

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/editoras', 'EditoraController@index');
 
 // Rota para url 'editoras/1', por exemplo
 Route::get('/editoras/{editora}', 'EditoraController@show');
+
+// Rota para url '/livros'
+Route::get('/livros', 'LivroController@index');
+
+// Rota para url 'livros/1', por exemplo
+Route::get('/livros/{livro}', 'LivroController@show');
